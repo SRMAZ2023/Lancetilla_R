70448fba8a07b5eaa6e3046708735839b688aab7
AlexProMaster <125411071+AlexProMaster@users.noreply.github.com>
2023-05-16 20:54:01 -0600

    Cambios

---
+++ La API/Lancetilla_R/Lancetilla.API/Extensions/MappingProfileExntensions.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Lancetilla.API.Controllers;
+
+namespace Lancetilla.API.Extensions
+{
+    public class MappingProfileExntensions : Profile
+    {
+        public MappingProfileExntensions()
+        {
+            //CreateMap<EmpleadoViewModel,                tbEmpleados>().ReverseMap();
+            //CreateMap<ClienteViewModel,                 tbClientes>().ReverseMap();
+          
+            //CreateMap<FacturaViewModel,                 tbFacturas>().ReverseMap();
+           
+            //CreateMap<FacturaDetalleViewModel,          tbFacturaDetalle>().ReverseMap();
+         
+            //CreateMap<RolPorPantallaViewModel,          tbPantallasPorRoles>().ReverseMap();
+
+
+            //CreateMap<DepartamentoViewModel,            tbDepartamentos>().ReverseMap();
+            //CreateMap<MunicipioViewModel,               tbMunicipios>().ReverseMap();
+          
+            //CreateMap<EstadoCivilViewModel,             tbEstadosCiviles>().ReverseMap();
+
+
+            //CreateMap<UsuarioViewModel,                 tbUsuarios>().ReverseMap();
+            //CreateMap<RolViewModel,                     tbRoles>().ReverseMap();
+        }
+
+    }
+}
